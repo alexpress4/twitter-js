86f3fe7c0b393243365df102e42da17dae28398b
Alex Press <press.alex@gmail.com>
2017-07-31 18:21:30 -0400

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    console.log(req.method, req.originalUrl);
+    // console.log("This is the RESPONSE object: ", res.method, res.originalUrl);
+    next();
+});
+
 app.listen(3000, function(){
   console.log('server listening');
+});
+
+app.get('/news', function(req, res){
+  res.send('news');
 });
 
 app.get('/', function(req, res){
