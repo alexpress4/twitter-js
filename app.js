1f288ff69784b307c87dc3a0783ac443969f599a
Alex Press <press.alex@gmail.com>
2017-08-01 12:01:15 -0400

    static routing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
+app.use('/', routes);
 
 
 app.use(function (req, res, next) {
@@ -12,14 +14,14 @@ app.use(function (req, res, next) {
 app.listen(3000, function(){
   console.log('server listening');
 });
+//
+// app.get('/news', function(req, res){
+//   res.send('news');
+// });
 
-app.get('/news', function(req, res){
-  res.send('news');
-});
-
-app.get('/', function(req, res){
-  res.send('Welcome');
-});
+// app.get('/', function(req, res){
+//   res.send('Welcome');
+// });
 
 var locals = {
     title: 'An Example',
@@ -32,13 +34,15 @@ var locals = {
 
 nunjucks.configure('views', {noCache: true})
 
-nunjucks.render('index.html', locals, function(err, output){
-  console.log(output);
-});
+// nunjucks.render('index.html', locals, function(err, output){
+//   console.log(output);
+// });
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-res.render( 'index', {title: 'Hall of Fame', people: people} );
+// app.get('/', function(req, res){
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
