e7817d51ae8e63f9091b8890895188c8fa0b0958
Alex Press <press.alex@gmail.com>
2017-08-01 11:06:41 -0400

    nunjucks

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
+
 
 app.use(function (req, res, next) {
     console.log(req.method, req.originalUrl);
@@ -18,3 +20,25 @@ app.get('/news', function(req, res){
 app.get('/', function(req, res){
   res.send('Welcome');
 });
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views', {noCache: true})
+
+nunjucks.render('index.html', locals, function(err, output){
+  console.log(output);
+});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+res.render( 'index', {title: 'Hall of Fame', people: people} );
